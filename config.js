d269d0e68f52ba202541ac33c0526a816c04d07a
ilken393 <139415274+ilken393@users.noreply.github.com>
2026-07-26 10:14:01 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    ownerNumber: "6285182359268@s.whatsapp.net",
+    ownerInstagram: "@instagram_owner",
+    ownerID: "ID_OWNER_123",
+    watermark: "Altra Assixtenc",
+    apis: {
+        // Taruh link API key kamu di sini
+        openai: "sk-xxxxx",
+        paymentGateway: "https://api.payment.com/qris"
+    },
+    fakeStatus: {
+        key: {
+            fromMe: false,
+            participant: '0@s.whatsapp.net',
+            remoteJid: 'status@broadcast'
+        },
+        message: {
+            imageMessage: {
+                jpegThumbnail: "",
+                caption: "Altra Assixtenc 💗🌷"
+            }
+        }
+    }
+};
